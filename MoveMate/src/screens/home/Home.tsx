c66e53d4e88f5a669b300bedc9381aa36e6c1b1f
Vera Zhao <yuwei.zhao@student.unsw.edu.au>
2023-10-04 14:31:59 +1100

    install ant design mobile

--- MoveMate/src/screens/home/Home.tsx
+++ MoveMate/src/screens/home/Home.tsx
@@ -1,15 +1,22 @@
-import { View, Text, Button } from 'react-native'
+// import { View, Text, Button } from 'react-native'
+import { View, Text, Image} from 'react-native'
 import React from 'react'
 import { useNavigation } from '@react-navigation/native';
 import { ROUTES } from '../../constants';
+import Button from '@ant-design/react-native/lib/button';
+
 
 const Home = () => {
   const navigation = useNavigation();
   return (
     <View className="flex flex-1 items-center justify-center">
       <Text>Home</Text>
-      <Button title="Home Detail" onPress={() => navigation.navigate(ROUTES.HOME_DETAIL) }/>
-    </View>
+      {/* <Button title="Home Detail" onPress={() => navigation.navigate(ROUTES.HOME_DETAIL) }/> */}
+
+      {/* ant design 组件 */}
+      <Button type="primary" onPress={() => navigation.navigate(ROUTES.HOME_DETAIL) }> Home detail</Button>
+      {/* <Checkbox>check test</Checkbox> */}
+      </View>
   )
 }
 
