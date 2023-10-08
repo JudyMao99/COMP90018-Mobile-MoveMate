86b0e1109a4d435966ecaee919244862167a2e4f
JudyMao <judymao99@gmail.com>
2023-10-08 14:31:10 +1100

    Auth ui done, firebase to add.

--- MoveMate/src/navigations/AuthNavigator.tsx
+++ MoveMate/src/navigations/AuthNavigator.tsx
@@ -10,7 +10,7 @@ const Stack = createStackNavigator();
 function AuthNavigator() {
   return (
     <Stack.Navigator screenOptions={{}} initialRouteName={ROUTES.LOGIN}>
-      <Stack.Screen name={ROUTES.LOGIN} component={Login} />
+      <Stack.Screen name={ROUTES.LOGIN} component={Login}  options={{ headerShown: false }} />
       <Stack.Screen name={ROUTES.FORGOT_PASSWORD} component={ForgotPassword}/>
       <Stack.Screen name={ROUTES.REGISTER} component={Register} />
       <Stack.Screen name={ROUTES.APP} component={BottomTabNavigator} options={{ headerShown: false }} />
