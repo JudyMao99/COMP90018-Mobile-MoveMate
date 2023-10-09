a73e249d6869d840ae88306889dc5bd9d82639e5
Ellen Zhuang <ellenzhuang@MacBook.local>
2023-10-09 22:01:04 +1100

    Created the components folder for the BarChart and LineChart components

--- MoveMate/src/screens/dashboard/Dashboard.tsx
+++ MoveMate/src/screens/dashboard/Dashboard.tsx
@@ -2,6 +2,8 @@ import { View, Text, Button } from 'react-native'
 import React from 'react'
 import { useNavigation } from '@react-navigation/native';
 import { ROUTES } from '../../constants';
+import BarChart from '../../components/BarChart';
+import LineChart from '../../components/LineChart';
 
 const Dashboard = () => {
   const navigation = useNavigation();
@@ -9,6 +11,8 @@ const Dashboard = () => {
   return (
     <View className="flex flex-1 items-center justify-center">
       <Text>Dashboard</Text>
+      <BarChart />
+      <LineChart />
       <Button title="Dashboard Detail" onPress={() => navigation.navigate(ROUTES.DASHBOARD_DETAIL) }/>
     </View>
   )
