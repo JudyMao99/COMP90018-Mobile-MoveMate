9d3b6ae50901dbfd00ae5df0bd00f8795faf3ca6
Vera Zhao <yuwei.zhao@student.unsw.edu.au>
2023-10-16 17:34:44 +1100

    set routes

--- MoveMate/src/navigations/AuthNavigator.tsx
+++ MoveMate/src/navigations/AuthNavigator.tsx
@@ -4,6 +4,8 @@ import BottomTabNavigator from './BottomTabNavigator';
 import Login from '../screens/auth/Login';
 import ForgotPassword from '../screens/auth/ForgotPassword';
 import Register from '../screens/auth/Register';
+import WorkingMode from '../screens/home/WorkingMode';
+import WorkingFinish from '../screens/home/WorkingFinish';
 
 const Stack = createStackNavigator();
 
@@ -14,6 +16,8 @@ function AuthNavigator() {
       <Stack.Screen name={ROUTES.FORGOT_PASSWORD} component={ForgotPassword}/>
       <Stack.Screen name={ROUTES.REGISTER} component={Register} />
       <Stack.Screen name={ROUTES.APP} component={BottomTabNavigator} options={{ headerShown: false }} />
+      <Stack.Screen name={ROUTES.WORKING_MODE} component={WorkingMode} initialParams={{duration: 15}} options={{ headerShown: false }}/>
+      <Stack.Screen name={ROUTES.WORKING_FINISH} component={WorkingFinish} options={{ headerShown: false }}/>
     </Stack.Navigator>
   );
 }
