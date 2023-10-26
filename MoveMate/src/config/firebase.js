b7153286e74e3109a257e8125d5e2ecc2621b9a7
JudyMao <judymao99@gmail.com>
2023-10-26 16:07:03 +1100

    Update firebase config.

--- MoveMate/src/config/firebase.js
+++ MoveMate/src/config/firebase.js
@@ -1,20 +1,20 @@
-// Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
 import { getAuth } from 'firebase/auth';
 
-// Your web app's Firebase configuration
 const firebaseConfig = {
   apiKey: "AIzaSyBaef6pxHW0SIzJYhPwqsbMk65lzmldxWI",
   authDomain: "mobile-movemate.firebaseapp.com",
   projectId: "mobile-movemate",
   storageBucket: "mobile-movemate.appspot.com",
   messagingSenderId: "303461517165",
-  appId: "1:303461517165:web:03601dc82341fdf33bfc64"
+  appId: "1:303461517165:web:03601dc82341fdf33bfc64",
 };
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
+const auth = getAuth(app);
 
-export const auth = getAuth(app);
+export { auth };
+
+// IOS: 567092706586-8tlk7ariv0erl08nb9bmn69mhva2ncof.apps.googleusercontent.com
+// Android: 567092706586-23l8rda3a1a2a4fcnrnss0u4ehpcbiob.apps.googleusercontent.com
