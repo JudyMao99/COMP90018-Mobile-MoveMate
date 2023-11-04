00e671595aca8bc066b7f1b9691857dc348b3896
ellenzhuang <ellenzhuang88@gmail.com>
2023-11-05 03:10:05 +1100

    Fetched the focus data from firestore and rendered it as a line chart

--- MoveMate/src/components/FocusChart.tsx
+++ MoveMate/src/components/FocusChart.tsx
@@ -1,28 +1,95 @@
+// import React from 'react';
+// import { Dimensions } from 'react-native';
+// import { LineChart } from 'react-native-chart-kit';
+// import { FocusData, FocusHistoryItem } from '../data';
+
+// function formatDate(input: string): string {
+//   const months = [
+//     'Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun',
+//     'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec'
+//   ];
+
+//   const date = new Date(input);
+//   const month = months[date.getMonth()];
+//   const day = date.getDate();
+
+//   return `${month} ${day}`;
+// }
+
+// const FocusChart = () => {
+//   return (
+//     <LineChart
+//       data={{
+//         labels: FocusData.focusHistory.map(item => formatDate(item.date)),
+//         datasets: [{
+//           data: FocusData.focusHistory.map(item => item.duration)
+//         }]
+//       }}
+//       width={Dimensions.get("window").width} 
+//       height={220}
+//       yAxisLabel=""
+//       yAxisSuffix=" min"
+//       yAxisInterval={1}
+//       chartConfig={{
+//         backgroundColor: "#e26a00",
+//         backgroundGradientFrom: "#fb8c00",
+//         backgroundGradientTo: "#ffa726",
+//         decimalPlaces: 0, 
+//         color: (opacity = 0.7) => `rgba(255, 255, 255, ${opacity})`,
+//         labelColor: (opacity = 1) => `rgba(255, 255, 255, ${opacity})`,
+//         style: {
+//           borderRadius: 16
+//         },
+//         propsForDots: {
+//           r: "6",
+//           strokeWidth: "2",
+//           stroke: "#ffa726"
+//         }
+//       }}
+//       bezier
+//       style={{
+//         marginVertical: 10,
+//         borderRadius: 16
+//       }}
+//     />
+//   );
+// };
+
+// export default FocusChart;
+
 import React from 'react';
 import { Dimensions } from 'react-native';
 import { LineChart } from 'react-native-chart-kit';
-import { FocusData, FocusHistoryItem } from '../data';
 
+// Define the props type to include the focus summary data
+type FocusChartProps = {
+  focusSummaryData: {
+    date: string;
+    totalDuration: number;
+  }[];
+};
+
+// Update the formatDate function to handle the MM-DD format
 function formatDate(input: string): string {
+  const [month, day] = input.split('-');
   const months = [
     'Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun',
     'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec'
   ];
 
-  const date = new Date(input);
-  const month = months[date.getMonth()];
-  const day = date.getDate();
+  // Convert month from MM format to month name
+  const monthName = months[parseInt(month, 10) - 1];
 
-  return `${month} ${day}`;
+  return `${monthName} ${parseInt(day, 10)}`;
 }
 
-const FocusChart = () => {
+const FocusChart: React.FC<FocusChartProps> = ({ focusSummaryData }) => {
   return (
     <LineChart
       data={{
-        labels: FocusData.focusHistory.map(item => formatDate(item.date)),
+        labels: focusSummaryData.map(item => formatDate(item.date)),
         datasets: [{
-          data: FocusData.focusHistory.map(item => item.duration)
+          data: focusSummaryData.map(item => item.totalDuration)
         }]
       }}
       width={Dimensions.get("window").width} 
--- MoveMate/src/screens/dashboard/Dashboard.tsx
+++ MoveMate/src/screens/dashboard/Dashboard.tsx
@@ -1,33 +1,96 @@
-import { View, Text, Button } from 'react-native'
-import React from 'react'
-import { useNavigation } from '@react-navigation/native';
-import { ROUTES } from '../../constants';
-import FocusStatsChart from '../../components/FocusChart';
-// import useAuth from '../../hook/useAuth';
-// import { User } from '../../config/firebase/auth';
+import React, { useEffect, useState } from 'react';
+import { View, Text } from 'react-native';
+import moment from 'moment';
+import { collection, query, where, getDocs, orderBy } from "firebase/firestore";
+import useAuth from '../../hook/useAuth';
+import { db } from '../../config/firebase';
+import FocusChart from '../../components/FocusChart';
+
+type FocusSummaryData = {
+  date: string;
+  totalDuration: number;
+};
 
 const Dashboard = () => {
-  const navigation = useNavigation();
+  const { user } = useAuth();
+  const [focusSummaryData, setFocusSummaryData] = useState<FocusSummaryData[]>([]);
 
-  // const fetchUserData = async (uid: string) => {
-  // const userDoc = await db.collection('users').doc(uid).get();
-  // if (userDoc.exists) {
-  //   const userData = userDoc.data();
-  //   // update state
-  // } else {
-  //   // handle user not exist
-  // }
+  useEffect(() => {
+    const fetchFocusSummary = async (uid: string): Promise<FocusSummaryData[]> => {
+      // Set the start date to 6 days before today
+      const start_date = moment().subtract(6, 'days').startOf('day');
+      // Set the end date to the end of today
+      const end_date = moment().endOf('day');
+
+      // Initialise the date range array
+      const dateRange: FocusSummaryData[] = [];
+      for (let m = moment(start_date); m.isSameOrBefore(end_date, 'day'); m.add(1, 'days')) {
+        dateRange.push({
+          date: m.format('MM-DD'),
+          totalDuration: 0,
+        });
+      }
+
+      console.log(`Date range for summary: ${dateRange.map(d => d.date).join(', ')}`);
+
+      const q = query(
+        collection(db, 'focus'),
+        where('uid', '==', uid),
+        where('start_date', '>=', start_date.toDate()),
+        where('start_date', '<=', end_date.toDate()),
+        orderBy('start_date')
+      );
+
+      try {
+        const querySnapshot = await getDocs(q);
+        console.log(`Documents found: ${querySnapshot.size}`);
+
+        querySnapshot.forEach((doc) => {
+          const data = doc.data();
+          const date: Date = data.start_date.toDate();
+          const duration: number = data.duration;
+          const dateKey = moment(date).format('MM-DD');
+
+          console.log(`Processing document for date: ${dateKey}, duration: ${duration}`);
+
+          // Find the corresponding date and accumulate the duration
+          const day = dateRange.find(d => d.date === dateKey);
+          if (day) {
+            day.totalDuration += duration;
+          }
+        });
+
+        return dateRange;
+      } catch (error) {
+        console.error("Error fetching focus summary: ", error);
+        return [];
+      }
+    };
+
+    if (user?.uid) {
+      (async () => {
+        const summaries = await fetchFocusSummary(user.uid);
+        console.log('Summaries received:', summaries);
+        setFocusSummaryData(summaries);
+      })();
+    }
+  }, [user?.uid]);
 
   return (
-    <View className="flex flex-1 items-center justify-center">
-      <Text>Dashboard</Text>
-      {/* <BarChart /> */}
-      <FocusStatsChart />
-      <Button title="Dashboard Detail" onPress={() => navigation.navigate(ROUTES.DASHBOARD_DETAIL) }/>
-      <Button title="Map" onPress={() => navigation.navigate(ROUTES.MAP) }/>
+    <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
+      <Text>Focus Summary (Last 7 Days)</Text>
+      {focusSummaryData.length > 0 ? (
+        focusSummaryData.map((data) => (
+          <Text key={data.date}>
+            {data.date}: {data.totalDuration} minutes
+          </Text>
+        ))
+      ) : (
+        <Text>No focus data available for the last 7 days.</Text>
+      )}
+      <FocusChart focusSummaryData={focusSummaryData} />
     </View>
-  )
-
+  );
 };
 
 export default Dashboard;
