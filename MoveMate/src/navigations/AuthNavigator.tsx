1ca36bf48473a3b5a6bead1d7e64b8702c24257b
JudyMao <judymao99@gmail.com>
2023-11-07 17:57:27 +1100

    Delete unused files

--- MoveMate/src/navigations/AuthNavigator.tsx
+++ MoveMate/src/navigations/AuthNavigator.tsx
@@ -1,7 +1,6 @@
 import { createStackNavigator } from '@react-navigation/stack';
 import { ROUTES } from '../constants';
 import Login from '../screens/auth/Login';
-import ForgotPassword from '../screens/auth/ForgotPassword';
 import Register from '../screens/auth/Register';
 import Welcome from '../screens/auth/Welcome';
 
@@ -13,7 +12,6 @@ function AuthNavigator() {
     <Stack.Navigator screenOptions={{ gestureEnabled: false }} initialRouteName={ROUTES.WELCOME} >
       <Stack.Screen name={ROUTES.WELCOME} component={Welcome} options={{ headerShown: false }} />
       <Stack.Screen name={ROUTES.LOGIN} component={Login} options={{ headerShown: false }} />
-      <Stack.Screen name={ROUTES.FORGOT_PASSWORD} component={ForgotPassword}  options={{ headerShown: false }} />
       <Stack.Screen name={ROUTES.REGISTER} component={Register} options={{ headerShown: false }} />
     </Stack.Navigator>
   );
