438132fef75f13b8844a14141cc6a9536ab49545
Vera Zhao <yuwei.zhao@student.unsw.edu.au>
2023-11-07 18:33:02 +1100

    add cycling distance and suration data in the home page

--- MoveMate/src/screens/home/WorkingFinish.tsx
+++ MoveMate/src/screens/home/WorkingFinish.tsx
@@ -87,7 +87,7 @@ const WorkingFinish = () => {
             </View>
             <View className='w-full top-40 '>
                 <Button 
-                    onPress={() => navigation.navigate(ROUTES.HOME_MAIN)}>back to home</Button>
+                    onPress={() => navigation.navigate(ROUTES.HOME_MAIN)}>Back to Home</Button>
             </View>
             
         </View>
