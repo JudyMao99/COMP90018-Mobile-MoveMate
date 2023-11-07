793c8201f969daf4b4396db2e325c95086a2b524
JudyMao <judymao99@gmail.com>
2023-11-07 17:50:07 +1100

    Change bottom bar icons

--- MoveMate/src/navigations/BottomTabNavigator.tsx
+++ MoveMate/src/navigations/BottomTabNavigator.tsx
@@ -7,6 +7,7 @@ import HomeNavigator from './HomeNavigator';
 import ProfileNavigator from './ProfileNavigator';
 import DashboardNavigator from './DashboardNavigator';
 import { Image } from 'react-native';
+import { Ionicons } from '@expo/vector-icons';
 import { RouteProp, ParamListBase, getFocusedRouteNameFromRoute } from '@react-navigation/native';
 import MaterialCommunityIcons from 'react-native-vector-icons/MaterialCommunityIcons';
 
@@ -15,40 +16,32 @@ const Stack = createStackNavigator();
 function BottomTabNavigator() {
 
   return (
-    <Tab.Navigator
-       screenOptions={{
-        headerShown: false,
-      }}
-    >
+    <Tab.Navigator screenOptions={{ headerShown: false }}>
       <Tab.Screen 
-      name={ROUTES.HOME_NAV} 
-      component={HomeNavigator} 
-      options={({route}) => ({
-          tabBarStyle: {
-            display: getTabBarVisibility(route),
-          },
-          tabBarIcon: ({ color, size }) => (
-            <MaterialCommunityIcons name="home" color={color} size={size} />
-          ),
-        })}
-        />
-      <Tab.Screen 
-        name={"History"} 
-        component={DashboardNavigator} 
-        options = {{
-          tabBarIcon: ({ color, size }) => (
-            <MaterialCommunityIcons name="chart-bar" color={color} size={size} />
-          ),
+        name={ROUTES.HOME_NAV} 
+        component={HomeNavigator} 
+        options={
+          ({ route }) => ({
+            tabBarStyle: { display: getTabBarVisibility(route) },
+            tabBarIcon: ({ color, size }) => <Ionicons name="home-sharp" color={color} size={size} />,
+            tabBarLabel: "Home",
+          })
+        }
+      />
+      <Tab.Screen
+        name={ROUTES.DASHBOARD}
+        component={DashboardNavigator}
+        options={{
+          tabBarIcon: ({ color, size }) => <Ionicons name="bar-chart-sharp" color={color} size={size} />,
+          tabBarLabel: "Dashboard",
         }}
       />
       <Tab.Screen
         name={ROUTES.PROFILE_NAVIGATION}
         component={ProfileNavigator}
         options={{
-          tabBarIcon: ({ color }) => (
-            <Image source={require('../assets/icons/profile.png')} style={{ tintColor: color }} />
-          ),
-          tabBarLabel: undefined,
+          tabBarIcon: ({ color, size }) => <Ionicons name="person-sharp" color={color} size={size} />,
+          tabBarLabel: "Profile",
         }}
       />
     </Tab.Navigator>
@@ -56,7 +49,7 @@ function BottomTabNavigator() {
 }
 
 // Get the current screen name and return nones when in specific page
-const getTabBarVisibility = (route) => {
+const getTabBarVisibility = (route: any) => {
   const routeName = getFocusedRouteNameFromRoute(route) ?? ROUTES.HOME_NAV;
   if( routeName == ROUTES.WORKING_MODE || routeName == ROUTES.WORKING_FINISH || routeName == ROUTES.WALKING_MODE) {
     return 'none';
